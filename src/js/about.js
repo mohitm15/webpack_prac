f8a1a3956bdc353bd785e93413993902fc261652
Mohit Maroliya <mmaroliya@gmail.com>
2020-02-28 07:46:50 +0530

    Adding multiple bundle and htmlwebpack plugin

---
+++ src/js/about.js
@@ -0,0 +1,1 @@
+console.log('THIS IS FROM ABOUT PAGE');
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    about: './src/js/about.js'
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Multiple Bundles',
+      }),
+    ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   devtool: 'inline-source-map',
